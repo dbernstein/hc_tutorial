88323fcea97b2f3af2d71f6bc4ab1b961a0094b7
Daniel Bernstein <dabylon@gmail.com>
2013-08-08 08:32:11 -0700

    Refactors generic_file_spec.rb

--- spec/models/generic_file_spec.rb
+++ spec/models/generic_file_spec.rb
@@ -2,20 +2,38 @@ require 'spec_helper'
 
 describe 'tags related changes' do
 
-  it 'should have a tags field' do
+  before(:each) do
+    @gf = GenericFile.new
+    @gf.tags = ["tag1", "tag2", "tag3"]
+  end
 
-    gf = GenericFile.new
-    gf.tags = ["tag1", "tag2", "tag3"]
+  it 'should have a tags field' do
+    @gf.respond_to?(:tags).should be_true
   end
 
   it 'should makes tags available' do
-    gf = GenericFile.new
-    true.should == gf.terms_for_display.include?(:tags)
- end
+    @gf.terms_for_display.include?(:tags).should be_true
+  end
 
   it 'should make tag unavailable' do
-    gf = GenericFile.new
-    false.should == gf.terms_for_display.include?(:tag)
+    @gf.terms_for_display.include?(:tag).should be_false
+  end
+
+  it 'should solrize tags field into array of words' do
+    solrdocs = {}
+    @gf.tags = ["one two three four five six seven"]
+    @gf.to_solr(solrdocs);
+
+    [
+        "fields_tags_tesim",
+        "fields_tags_sim",
+        "tags_tesim",
+        "tags_sim"
+    ].each { |x|
+        solrdocs.keys.include?(x).should be_true
+    }
+
+    solrdocs["fields_tags_tesim"].count.should eq(7)
   end
 
 end
